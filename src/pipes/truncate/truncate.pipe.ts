83683c2775c0181a844fb4f2561585ebf773953e
Taras Kovalenko <bad.kto@gmail.com>
2018-01-31 15:08:58 +0200

    (pipe) truncate - init gitignore - updated

---
+++ src/pipes/truncate/truncate.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'truncate'
+})
+export class TruncatePipe implements PipeTransform {
+
+  transform(value: string, limit: number = 40, trail: String = '…'): string {
+    if (!value) {
+        return '';
+    }
+
+    if (limit < 0) {
+      limit *= -1;
+      return value.length > limit ? `${value.substring(value.length - limit, value.length)}${trail}` : value;
+    }
+
+    return value.length > limit ? `${value.substring(0, limit)}${trail}` : value;
+  }
+
+}
